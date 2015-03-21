32343d8e1881faa832485be07add995367c134b4
narendrasoni1989 <narendrasoni2@gmail.com>
2015-03-21 23:55:41 +0800

    users REST routes added

--- mean-workshop-1/routes/users.js
+++ mean-workshop-1/routes/users.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  res.json({msg:'response from GET /user'});
+});
+/* CREATE new User */
+router.post('/',function(req,res){
+        res.json({msg:'response from POST /user'});
+});
+
+/* GET a user with id */
+router.get('/:id', function(req, res) {
+  res.json({msg:'response from GET /user/:id'});
+});
+
+/* UPDATE user with id */
+router.put('/:id', function(req, res) {
+  res.json({msg: 'response from PUT /user/:id'});
+});
+
+router.delete('/:id',function(req,res){
+  res.json({msg: 'response from DELETE /user/:id'});
 });
 
 module.exports = router;
