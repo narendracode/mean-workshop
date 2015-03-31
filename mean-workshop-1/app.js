1ecc86185c60da7ffc1c51fafe51a7c91bf60dba
narendrasoni1989 <narendrasoni2@gmail.com>
2015-03-31 22:44:07 +0800

    corrected typo in app.js

--- mean-workshop-1/app.js
+++ mean-workshop-1/app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-require('./config/routes')(app);
+require('./config/express')(app);
 require('./config/routes')(app);
 
 module.exports = app;
