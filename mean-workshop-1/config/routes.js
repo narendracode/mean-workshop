50aabbca937eed2aaae36b24cee84d835f6f82da
narendrasoni1989 <narendrasoni2@gmail.com>
2016-06-30 13:47:47 +0800

    mean-workshop1 updated to new version

--- mean-workshop-1/config/routes.js
+++
@@ -1,7 +0,0 @@
-var index = require('../routes/index');
-var users = require('../routes/users');
-
-module.exports = function(app){
-        app.use('/',index);
-        app.use('/users',users);
-}
--- mean-workshop-1/routes/users.js
+++ mean-workshop-1/routes/users.js
@@ -2,26 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.json({msg:'response from GET /user'});
-});
-/* CREATE new User */
-router.post('/',function(req,res){
-        res.json({msg:'response from POST /user'});
-});
-
-/* GET a user with id */
-router.get('/:id', function(req, res) {
-  res.json({msg:'response from GET /user/:id'});
-});
-
-/* UPDATE user with id */
-router.put('/:id', function(req, res) {
-  res.json({msg: 'response from PUT /user/:id'});
-});
-
-router.delete('/:id',function(req,res){
-  res.json({msg: 'response from DELETE /user/:id'});
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
 module.exports = router;
